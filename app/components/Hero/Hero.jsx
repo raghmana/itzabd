bc0fbce35d7fc6ed4c2c35479d1f38ac877a48b1
abd <raghman.gem@gmail.com>
2025-10-26 12:55:02 +0530

    icons added on hero section

--- app/components/Hero/Hero.jsx
+++ app/components/Hero/Hero.jsx
@@ -1,5 +1,30 @@
 import styles from './Hero.module.scss';
 import Link from 'next/link';
+import { 
+  FaHtml5, 
+  FaCss3Alt, 
+  FaJs, 
+  FaReact, 
+  FaBrain,
+  FaPython, 
+  FaSass,
+  FaWordpress,
+  FaShopify,
+  FaFigma,
+  FaGitAlt,
+  FaAws
+} from 'react-icons/fa';
+import { 
+  SiRedux,
+  SiNextdotjs,
+  SiMongodb,
+  SiOpenai,
+  SiHuggingface,
+  SiLangchain
+} from 'react-icons/si';
+import { TbApi } from 'react-icons/tb';
+import { GiArtificialIntelligence, GiProcessor } from 'react-icons/gi';
+import { VscGraphLine } from 'react-icons/vsc';
 
 export default function Hero() {
   return (
@@ -22,6 +47,34 @@ export default function Hero() {
             <Link href="#projects" className="btn btn-secondary">
               View Projects
             </Link>
+          </div>
+          <div className={styles.hero__tech}>
+            <div className={styles.hero__tech_group}>
+              <div className={styles.hero__tech_icons}>
+                <FaHtml5 title="HTML5" />
+                <FaCss3Alt title="CSS3" />
+                <FaJs title="JavaScript" />
+                <FaReact title="React" />
+                <SiNextdotjs title="Next.js" />
+                <FaSass title="Sass" />
+                <SiRedux title="Redux" />
+                <FaFigma title="Figma" />
+                <FaBrain title="AI" />
+                <SiOpenai title="OpenAI" />
+                <GiArtificialIntelligence title="CrewAI" />
+                <SiHuggingface title="Hugging Face" />
+                <SiLangchain title="LangChain" />
+                <GiProcessor title="AgentticAI" />
+                <VscGraphLine title="RAG" />
+                <FaPython title="Python" />
+                <SiMongodb title="MongoDB" />
+                <TbApi title="REST API" />
+                <FaWordpress title="WordPress" />
+                <FaShopify title="Shopify" />
+                <FaGitAlt title="Git" />
+                <FaAws title="AWS" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
